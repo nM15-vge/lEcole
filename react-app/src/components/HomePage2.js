24206aa8e0f42d795be0cbab0234659a4fbbf518
Mauro Sanchez <novamauro98@gmail.com>
2021-05-13 11:58:01 -0500

    Started to look like an actual room.

--- react-app/src/components/HomePage2.js
+++ react-app/src/components/HomePage2.js
@@ -4,12 +4,12 @@ const HomePage2 = () => {
     return (
         <div className="container">
             <div className="cube pers650 center-flex">
-                <div className="right"></div>
-                <div className="back">Ba</div>
-                <div className="left">L</div>
+                {/* <div className="right"></div> */}
+                <div className="left posBookshelfY"></div>
+                <div className="left posBookshelfX"></div>
                 <div className="face bottom"></div>
             </div>
-            <div id="floor"></div>
+            {/* <div id="floor"></div> */}
         </div>
     )
 };
