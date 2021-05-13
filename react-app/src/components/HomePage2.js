b4676fdf3d3e70655283aaee984c762ee2b441e8
Mauro Sanchez <novamauro98@gmail.com>
2021-05-13 14:00:04 -0500

    Added more pieces to the homepage.

--- react-app/src/components/HomePage2.js
+++ react-app/src/components/HomePage2.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 const HomePage2 = () => {
+    const Notebook = require('../assets/Notebook.svg')
     return (
         <div className="container">
             <div className="cube pers650 center-flex">
-                {/* <div className="right"></div> */}
-                <div className="left posBookshelfY"></div>
+                <div className="right"></div>
+                <div className="left posBookshelfY"><img className="notebook" src={Notebook}/></div>
                 <div className="left posBookshelfX"></div>
                 <div className="face bottom"></div>
             </div>
